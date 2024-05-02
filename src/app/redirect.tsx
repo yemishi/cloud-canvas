c3e0c30659c4384ba3f2b2e0f1997125f8eed5e2
Davi Damião <daviddperera10@gmail.com>
2024-05-02 22:42:43 +0100

    removed unecessary file

--- src/app/redirect.tsx
+++
@@ -1,8 +0,0 @@
-"use server";
-import { redirect } from "next/navigation";
-
-export async function navigate(location: string) {
-  if (!location) return;
-  const parseToUrl = encodeURI(location);
-  redirect(`/${parseToUrl}`);
-}
